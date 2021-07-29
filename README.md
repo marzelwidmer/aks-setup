996f52d9adfe07703aba4cdd4f20e04564ce153b
Marcel Widmer <marzelwidmer@gmail.com>
2021-07-29 10:42:18 +0200

    refactor script

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
-# Create Namespace
-```bash
- k create ns kboot
+# Manage AKS Cluster
+``` 
+--------------------------------------------
+:: Create or manage your AKS cluster
+--------------------------------------------
+ 1) AZ Login
+ 2) Create Resource Group and new AKS Cluster
+ 3) Delete Resource Group and AKS Cluster
+ 4) Start Cluster
+ 5) Stop Cluster
+ 6) Show Cluster
+ 7) Show Public-IP
+ 8) Update .kube/config
+ 9) Show Nodes
+10) Create DEMO Namespace
+11) Delete DEMO Namespace
+12) Quit
+Please enter your choice:
 ```
 
-# Switch to Namespace
+Please aware the number can be different.
 ```bash
-kns kboot
+./scripts/aks-cluster.sh
 ```
+
+
+
+
+
+
+
 
 # Install Infrastructure Resources
 ```bash
@@ -28,11 +50,11 @@ helm install prometheus --set prometheus.prometheusSpec.serviceMonitorSelectorNi
 
 ## Install Secrets
 ```bash
-kubesec decrypt secret/secret.enc.yaml | kubectl apply -n kboot -f -
+kubesec decrypt secret/secret.enc.yaml | kubectl apply -n demo -f -
 ```
 ### Check Secrets
 ```bash
-k get secrets -l 'secret in (kboot-infra)' -n kboot -ojson | jq '.items[].data'
+k get secrets -l 'secret in (kboot-infra)' -n demo -ojson | jq '.items[].data'
 ```
 
 ## Install RBAC for ConfigMaps/Secrets
@@ -47,8 +69,20 @@ https://ichi.pro/de/traefik-ingress-im-azure-kubernetes-dienst-275193782592692
 ```bash
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
+```
+
+## Update configuration with PublicIP
+Update `service.spec.loadBalancerIP` in `traefik-values.yaml` file 
+```bash
+./scripts/aks-cluster.sh
+vi traefik/helm/traefik-values.yaml
+```
+## Install Traefik 
+```bash
 helm install traefik traefik/traefik -f traefik/helm/traefik-values.yaml
 ```
+
+
 ## Check Installation
 ```bash
 kubectl get pods | grep traefik
--- scripts/aks-cluster.sh
+++ scripts/aks-cluster.sh
@@ -1,36 +1,232 @@
 #!/usr/bin/env bash
 
-read -p "Enter Name for a new Resource Group on Azure [AKS]: " AKS_RESOURCE_GROUP
-export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
-read -p "Enter Cluster Name [aks-cluster-c3smonkey]: " AKS_CLUSTER_NAME
-export AKS_CLUSTER_NAME=${AKS_CLUSTER_NAME:-aks-cluster-c3smonkey}
-read -p "Enter Name of Public IP Resource [aks-public-ip-c3smonkey]: " PIP_RESOURCE_GROUP
-export PIP_RESOURCE_GROUP=${PIP_RESOURCE_GROUP:-aks-public-ip-c3smonkey}
+echo "--------------------------------------------"
+echo ":: Create or manage your AKS cluster"
+echo "--------------------------------------------"
+
+
 PS3='Please enter your choice: '
-options=("Start Cluster" "Stop Cluster" "Show Cluster" "Show Public-IP" "Quit")
+options=(
+    "AZ Login"
+    "Create Resource Group and new AKS Cluster"
+    "Delete Resource Group and AKS Cluster"
+    "Start Cluster"
+    "Stop Cluster"
+    "Show Cluster"
+    "Show Public-IP"
+    "Update .kube/config"
+    "Show Nodes"
+    "Create DEMO Namespace"
+    "Delete DEMO Namespace"
+    "Quit"
+)
 select opt in "${options[@]}"
 do
     case $opt in
-        "Start Cluster")
-            echo "Start Cluster"
-            az aks stop -g $AKS_RESOURCE_GROUP -n $AKS_CLUSTER_NAME
+        "AZ Login")
+            echo ""
+            echo ": AZ Login :"
+            echo "------------"
+            az login --use-device-code
             break
             ;;
-        "Stop Cluster")
-            echo "Stop Cluster"
+
+        "Create Resource Group and new AKS Cluster")
+            echo ""
+            echo ": Create Cluster :"
+            echo "------------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+
+            read -p "Enter Cluster Name [aks-cluster-c3smonkey]: " AKS_CLUSTER_NAME
+            export AKS_CLUSTER_NAME=${AKS_CLUSTER_NAME:-aks-cluster-c3smonkey}
+
+            read -p "Enter Public IP Resource Name [aks-public-ip-c3smonkey]: " PIP_RESOURCE_GROUP
+            export PIP_RESOURCE_GROUP=${PIP_RESOURCE_GROUP:-aks-public-ip-c3smonkey}
+
+            read -p "Enter Container Registry [c3smonkey]: " CONTAINER_REGISTRY
+            export CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-c3smonkey}
+
+            read -p "Enter Location [eastus2]: " LOCATION
+            export LOCATION=${LOCATION:-eastus2}
+
+            read -p "Enter Node size [Standard_D1_V2]: " NODE_SIZE
+            export NODE_SIZE=${NODE_SIZE:-Standard_D1_V2}
+
+            read -p "Enter Node count [2]: " NODE_COUNT
+            export NODE_COUNT=${NODE_COUNT:-2}
+
+
+
+            az aks create --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --node-count $NODE_COUNT --node-vm-size $NODE_SIZE --enable-addons monitoring --generate-ssh-keys
+            echo "AKS CLuster $AKS_CLUSTER_NAME created with $NODE_COUNT nodes"
+
+            az network public-ip create \
+                          --resource-group $AKS_RESOURCE_GROUP \
+                          --name $PIP_RESOURCE_GROUP \
+                          --sku Standard \
+                          --allocation-method static
+            echo "Public IP created for $AKS_RESOURCE_GROUP with name $PIP_RESOURCE_GROUP"
+
+            CLIENT_ID=$(az aks show --name $AKS_CLUSTER_NAME --resource-group $AKS_RESOURCE_GROUP | jq -r .identity.principalId)
+            SUB_ID=$(az account show --query "id" --output tsv)
+
+            echo "Assign role for $CLIENT_ID and subscriptions $SUB_ID for $AKS_RESOURCE_GROUP"
+            az role assignment create --role "Virtual Machine Contributor" --assignee $CLIENT_ID --scope /subscriptions/$SUB_ID/resourceGroups/$AKS_RESOURCE_GROUP
+
+            echo "Role assigned for subscriptions $SUB_ID"
+            echo "Show public IP $PIP_RESOURCE_GROUP for cluster $AKS_RESOURCE_GROUP"
+            az network public-ip show -g $AKS_RESOURCE_GROUP -n $PIP_RESOURCE_GROUP | jq .ipAddress -r
+
+            az acr create --resource-group $AKS_RESOURCE_GROUP --name $CONTAINER_REGISTRY --sku Standard
+            az aks update -n $AKS_CLUSTER_NAME -g $AKS_RESOURCE_GROUP --attach-acr $CONTAINER_REGISTRY
+
+            echo "Enjoy your AKS Cluster"
+            az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+
+            echo "Update local ~/.kube/config for cluster $AKS_CLUSTER_NAME"
+            echo "you can login to the Container Registry with :"
+            echo "az acr login --name $CONTAINER_REGISTRY "
+
+            break
+            ;;
+        "Delete Resource Group and AKS Cluster")
+            echo ""
+            echo ": Delete Resource Group :"
+            echo "-------------------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+            az group delete --name $AKS_RESOURCE_GROUP
+            break
+            ;;
+
+
+        "Start Cluster")
+            echo ""
+            echo ": Start Cluster :"
+            echo "-----------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+
+            read -p "Enter Cluster Name [aks-cluster-c3smonkey]: " AKS_CLUSTER_NAME
+            export AKS_CLUSTER_NAME=${AKS_CLUSTER_NAME:-aks-cluster-c3smonkey}
+
             az aks start -g $AKS_RESOURCE_GROUP -n $AKS_CLUSTER_NAME
             break
             ;;
+
+        "Stop Cluster")
+            echo ""
+            echo ": Stop Cluster :"
+            echo "----------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+
+            read -p "Enter Cluster Name [aks-cluster-c3smonkey]: " AKS_CLUSTER_NAME
+            export AKS_CLUSTER_NAME=${AKS_CLUSTER_NAME:-aks-cluster-c3smonkey}
+
+            az aks stop -g $AKS_RESOURCE_GROUP -n $AKS_CLUSTER_NAME
+            break
+            ;;
+
         "Show Cluster")
-            echo "Show Cluster"
+            echo ""
+            echo ": Show Cluster: "
+            echo "----------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+
+            read -p "Enter Cluster Name [aks-cluster-c3smonkey]: " AKS_CLUSTER_NAME
+            export AKS_CLUSTER_NAME=${AKS_CLUSTER_NAME:-aks-cluster-c3smonkey}
+
             az aks show --name $AKS_CLUSTER_NAME --resource-group  $AKS_RESOURCE_GROUP
             break
             ;;
+
          "Show Public-IP")
-            echo "Show Public-IP"
+            echo ""
+            echo ": Show Public-IP :"
+            echo "------------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+
+            read -p "Enter Public IP Resource Name [aks-public-ip-c3smonkey]: " PIP_RESOURCE_GROUP
+            export PIP_RESOURCE_GROUP=${PIP_RESOURCE_GROUP:-aks-public-ip-c3smonkey}
+
             az network public-ip show -g $AKS_RESOURCE_GROUP -n $PIP_RESOURCE_GROUP | jq .ipAddress -r
             break
             ;;
+
+          "Update .kube/config")
+            echo ""
+            echo ": Copy Credentials from AKS :"
+            echo "-----------------------------"
+
+            read -p "Enter Resource Group Name [AKS]: " AKS_RESOURCE_GROUP
+            export AKS_RESOURCE_GROUP=${AKS_RESOURCE_GROUP:-AKS}
+
+            read -p "Enter Cluster Name [aks-cluster-c3smonkey]: " AKS_CLUSTER_NAME
+            export AKS_CLUSTER_NAME=${AKS_CLUSTER_NAME:-aks-cluster-c3smonkey}
+
+            az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+            echo "~/.kube/config for cluster $AKS_CLUSTER_NAME is updated"
+            break
+            ;;
+
+         "Show Nodes")
+            echo ""
+            echo ": Show Nodes :"
+            echo "-------------------------"
+            kubectl get nodes
+            break
+            ;;
+
+         "Create DEMO Namespace")
+            echo ""
+            echo ": Create Namespace DEMO :"
+            echo "-------------------------"
+            echo ""
+            echo ": Current Namespaces :"
+            echo ""
+            kubectl get ns
+            echo ""
+            echo ": Perform Create Namespace :"
+            kubectl create ns demo
+            kubectl config set-context --current --namespace demo
+            echo ""
+            echo ": Current Namespaces :"
+            echo ""
+            kubectl get ns
+            break
+            ;;
+
+         "Delete DEMO Namespace")
+            echo ""
+            echo ": Delete Namespace DEMO :"
+            echo "-------------------------"
+            echo ""
+            echo ": Current Namespaces :"
+            echo ""
+            kubectl get ns
+            echo ""
+            echo ": Perform Delete Namespace :"
+            echo ""
+            kubectl delete ns demo
+            echo ""
+            echo ": Current Namespaces :"
+            echo ""
+            kubectl get ns
+            break
+            ;;
+
+
+
         "Quit")
             break
             ;;
